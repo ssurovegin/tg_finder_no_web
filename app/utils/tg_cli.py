1f448ca820665ba9e29569923c272d76181a67e1
ssurovegin <serg.surovegin@gmail.com>
2025-05-19 18:21:22 +0400

    Update tg_cli.py

--- app/utils/tg_cli.py
+++ app/utils/tg_cli.py
@@ -4,14 +4,16 @@ from telethon import TelegramClient
 from dotenv import find_dotenv, load_dotenv
 
 
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(), override=True)
+
+SESSION_PATH = os.getenv('SESSION_PATH')
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
 
 
-
-client = TelegramClient('finder', os.getenv('API_ID'), os.getenv('API_HASH'))
-
-async def start_client():
-    await client.start()
+client = TelegramClient(SESSION_PATH, API_ID, API_HASH,
+                        device_model='iPhone 13', system_version='iOS 18',
+                        app_version='11.11')
 
 def get_telephone_client() -> TelegramClient:
     return client
